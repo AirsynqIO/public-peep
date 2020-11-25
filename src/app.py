a86adc54181efdf5daa2c459563ed540f5dc4ca4
favour <favouroked@gmail.com>
2020-11-25 14:54:17 +0100

    -> add video capture string

--- src/app.py
+++ src/app.py
@@ -1,10 +1,12 @@
 from src.core.video import Capture
 from src.services import assets
 
+captureSet = "gst-launch-1.0 v4l2src device=/dev/video0 ! video/x-raw, framerate=24/1 ! videoconvert ! autovideosink"
+
 
 def main():
     assets.init()
-    video_proc = Capture(0)
+    video_proc = Capture(captureSet)
     video_proc.start()
 
 
