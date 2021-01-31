c2da938d57827bc4fea2d2025815c670068181fd
favour <favouroked@gmail.com>
2021-01-31 18:30:40 +0100

    -> send frames and meta data

--- src/core/kafka_producer.py
+++ src/core/kafka_producer.py
@@ -1,13 +1,14 @@
 from kafka import KafkaProducer
+import json
 
 from src.config import RPI_ASSIGNED_ID
 
-imagesProducer = KafkaProducer(bootstrap_servers="3.14.84.232:9092")
+imagesProducer = KafkaProducer(bootstrap_servers="3.137.195.2:9092")
 images_topic = "camera-split-images"
 
 
 def send_image(frame):
     # todo: location data will be updated later
-    data = {'frame': frame, 'location': {}, 'assigned_id': RPI_ASSIGNED_ID}
-    imagesProducer.send(images_topic, data)
+    meta_data = {'location': {}, 'assigned_id': RPI_ASSIGNED_ID}
+    imagesProducer.send(images_topic, value=frame, key=json.dumps(meta_data).encode())
     imagesProducer.flush()
